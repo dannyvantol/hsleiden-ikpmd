ac1513cc01c315e0d53028bb244b4d48ccbcb7fd
Danny van Tol <danny@Saraato.home>
2018-09-24 10:01:04 +0200

    Initial Commit

---
+++ app/src/main/java/org/bitbucket/dannyvantol/rekenlokaal/SplashActivity.java
@@ -0,0 +1,22 @@
+package org.bitbucket.dannyvantol.rekenlokaal;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        }, 1000);
+    }
+}
