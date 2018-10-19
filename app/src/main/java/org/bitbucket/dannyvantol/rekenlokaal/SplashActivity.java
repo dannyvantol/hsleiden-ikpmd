948bb5fbba675ecb127a4e689548183188c880b5
Danny van Tol <danny.van.tol@outlook.com>
2018-10-19 16:28:30 +0200

    Add font and splashscreen

--- app/src/main/java/org/bitbucket/dannyvantol/rekenlokaal/SplashActivity.java
+++ app/src/main/java/org/bitbucket/dannyvantol/rekenlokaal/SplashActivity.java
@@ -4,23 +4,13 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
-import org.bitbucket.dannyvantol.rekenlokaal.fonts.FontCollection;
 
 public class SplashActivity extends AppCompatActivity {
-
-    TextView splash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
-        FontCollection fontCollection = FontCollection.getInstance(this);
-
-
-        splash = (TextView) findViewById(R.id.splash);
-        splash.setTypeface(fontCollection.candyCane);
     }
 
     @Override
